2d84aa5324c98a2e2c342aa2f69b6788691462bb
imnithin <nithinkool14@gmail.com>
2016-04-08 19:11:10 +0530

    commit missed files

---
+++ app/controllers/api/base_controller.rb
@@ -0,0 +1,12 @@
+class Api::BaseController < ApplicationController
+  protect_from_forgery with: :null_session
+  before_action :destroy_session
+
+  def destroy_session
+    request.session_options[:skip] = true
+  end
+
+  def not_found
+    render json: { 'message' => 'page not found' }, status: 404
+  end
+end
---
+++ app/controllers/api/emergencies_controller.rb
@@ -0,0 +1,30 @@
+class Api::EmergenciesController < Api::BaseController
+
+  def new
+    not_found
+  end
+  
+  def create
+    emergency = Emergency.create(emergency_parameters)
+    unless emergency.errors.any?
+      render json: emergency
+    else
+      render json: emergency.errors.full_messages
+    end
+  end
+
+  def show
+    emergency =   Emergency.find_by_id(params[:id])
+    if emergency
+      render json: emergency
+    else
+      #todo: record not found
+    end
+  end
+
+  private
+
+  def emergency_parameters
+    params.require(:emergency).permit(:fire_severity, :police_severity, :medical_severity)
+  end
+end
