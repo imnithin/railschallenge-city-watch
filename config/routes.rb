7180fccf03355d7c8e69f3ccd696067ed011d0a0
imnithin <nithinkool14@gmail.com>
2016-04-08 19:10:44 +0530

    removing versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1 do
-      resources :emergencies, only: [:create, :show, :new]
-    end
+    resources :emergencies, only: [:create, :show, :new]
   end
 end
