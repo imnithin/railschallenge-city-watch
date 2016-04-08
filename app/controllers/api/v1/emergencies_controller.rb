6fd4f76494a82b27fa55f18140195f4bc786f6e2
imnithin <nithinkool14@gmail.com>
2016-04-08 18:48:31 +0530

    not found GET /new failing

--- app/controllers/api/v1/emergencies_controller.rb
+++ app/controllers/api/v1/emergencies_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::EmergenciesController < Api::V1::BaseController
 
+  def new
+    not_found
+  end
+
   def create
     emergency = Emergency.create(emergency_parameters)
     unless emergency.errors.any?
@@ -22,6 +26,10 @@ class Api::V1::EmergenciesController < Api::V1::BaseController
 
   def emergency_parameters
     params.require(:emergency).permit(:fire_severity, :police_severity, :medical_severity)
+  end
+
+  def not_found
+    render json: { 'message' => 'page not found' }, status: 404
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :emergencies, only: [:create, :show]
+      resources :emergencies, only: [:create, :show, :new]
     end
   end
 end
